e5ebf86ddf69eeb31d68909ea61026733e7e8d78
Hudson-Meg <117606582+Hudson-Meg@users.noreply.github.com>
2022-11-12 14:24:21 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ let iBallX = 2;
 let iBallY = 0;
 let iBallDirectionX = -1;
 let iBallDirectionY = 1;
+let iFieldSize = 5;
 
 drawPaddle();
 drawBall();
@@ -12,7 +13,7 @@ drawBall();
 // Move paddle to the left when button A is clicked:
 input.onButtonPressed(Button.A, function () {
     if (iPaddleX > 0) {
-        iPaddleX += -1;
+        iPaddleX -= 1;
     } else {
         // Don't fall off the left edge of the world!
         iPaddleX = 0;
@@ -22,11 +23,11 @@ input.onButtonPressed(Button.A, function () {
 
 // Move paddle to the right when button B is clicked:
 input.onButtonPressed(Button.B, function () {
-    if (iPaddleX < 3) {
+    if (iPaddleX < (iFieldSize - iPaddleWidth)) {
         iPaddleX += 1;
     } else {
         // Don't fall off the right edge of the world!
-        iPaddleX = 3;
+        iPaddleX = iFieldSize - iPaddleWidth;
     }
     drawPaddle();
 })
@@ -48,22 +49,42 @@ function drawBall() {
 basic.forever(function() {
     
     basic.pause(100);
-    led.unplot(iBallX, iBallY);
 
-    // Move ball in the X and Y directions:
-
-    if ((iBallX >= 4 && iBallDirectionX > 0) ||
+    // To move the ball, start by turning off its last LED (unplotting):
+    // (Except when that LED is already used to draw the paddle!)
+    if (iBallY == (iFieldSize - 1) && (iBallX >= iPaddleX && iBallX <= (iPaddleX + iPaddleWidth - 1))) {
+        // We just hit the paddle: don't unplot!
+    } else {
+        led.unplot(iBallX, iBallY);
+    }
+    
+    // Handle bouncing off of left and right walls:
+    if ((iBallX >= (iFieldSize - 1) && iBallDirectionX > 0) ||
         (iBallX <= 0 && iBallDirectionX < 0)) {
         playWallBounceSound();
         iBallDirectionX *= -1; // Bounce back opposite!
     }
-
-    if ((iBallY >= 4 && iBallDirectionY > 0) ||
-        (iBallY <= 0 && iBallDirectionY < 0)) {
+    
+    // Handle bouncing off top wall:
+    if (iBallY <= 0 && iBallDirectionY < 0) {
         playWallBounceSound();
         iBallDirectionY *= -1; // Bounce back opposite!
     }
 
+    // Handle ball hitting bottom wall:
+    if (iBallY >= (iFieldSize - 1) && iBallDirectionY > 0) {
+        // Did we hit the paddle?
+        if (iBallX >= iPaddleX && iBallX <= (iPaddleX + iPaddleWidth - 1)) {
+            playPaddleBounceSound();
+            iBallDirectionY *= -1; // Bounce back opposite!
+        } else {
+            // We hit the bottom wall without the paddle: game over!
+            pongGameOver();
+            game.gameOver();
+        }
+    }
+
+    // Handle ball movement:
     iBallX = iBallX + iBallDirectionX;
     iBallY = iBallY + iBallDirectionY;
   
@@ -71,7 +92,22 @@ basic.forever(function() {
 })
 
 function playWallBounceSound() {
-    music.playTone(600, 30);
+    music.playTone(600, 60);
     basic.pause(150);
     music.playTone(450, 70);
+}
+
+function playPaddleBounceSound() {
+    music.playTone(450, 30);
+    basic.pause(80);
+    music.playTone(750, 45);
+}
+
+function pongGameOver() {
+    music.playTone(600, 60);
+    music.playTone(700, 40);
+    basic.pause(250);
+    music.playTone(500, 220);
+    basic.pause(350);
+    music.playTone(300, 870);
 }
